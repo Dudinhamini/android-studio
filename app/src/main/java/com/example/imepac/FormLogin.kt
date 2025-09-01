5a1cbb2bb5147dc8a4d552682e59401fee4b6afb
unknown <232-000985@IMEPACAD.local>
2025-09-01 20:22:13 -0300

    segundo commit

--- app/src/main/java/com/example/imepac/FormLogin.kt
+++ app/src/main/java/com/example/imepac/FormLogin.kt
@@ -11,6 +11,7 @@ class FormLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_form_login)
+        supportActionBar?.hide()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
