3976aa00ac62f31c919e46f95d91c24a87192b5e
unknown <232-000985@IMEPACAD.local>
2025-09-02 21:27:16 -0300

    primeiras 3 telas prontas

--- app/src/main/java/com/example/imepac/FormLogin.kt
+++ app/src/main/java/com/example/imepac/FormLogin.kt
@@ -1,6 +1,9 @@
 package com.example.imepac
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +14,11 @@ class FormLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_form_login)
-        supportActionBar?.hide()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        getSupportActionBar()?.hide();
+        val linkFormCadastro=findViewById<TextView>(R.id.text_tela_cadastro)
+        linkFormCadastro.setOnClickListener{
+            val telaFormCadastro = Intent(this, FormCadastro::class.java)
+            startActivity(telaFormCadastro)
         }
     }
 }
