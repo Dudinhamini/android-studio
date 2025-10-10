437cbe3f1c6729064d9cf40410a0fe015fb789c8
unknown <232-000985@IMEPACAD.local>
2025-10-09 21:44:41 -0300

    PQP

--- app/src/main/java/com/example/imepac/FormCadastro.kt
+++ app/src/main/java/com/example/imepac/FormCadastro.kt
@@ -5,48 +5,63 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class FormCadastro : AppCompatActivity() {
-    private lateinit var edit_nome: EditText
-    private lateinit var edit_email: EditText
-    private lateinit var edit_senha: EditText
-    private lateinit var btn_Cadastrar: Button
+
+    private lateinit var editNome: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editSenha: EditText
+    private lateinit var btnCadastrar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_form_cadastro)
         supportActionBar?.hide()
 
-        edit_nome = findViewById(R.id.edit_nome)
-        edit_email = findViewById(R.id.edit_email)
-        edit_senha = findViewById(R.id.edit_senha)
-        btn_Cadastrar = findViewById(R.id.bt_cadastro)
+        // Ligando os elementos do XML com as variáveis
+        editNome = findViewById(R.id.edit_nome)
+        editEmail = findViewById(R.id.edit_email_login)
+        editSenha = findViewById(R.id.edit_senha_login)
+        btnCadastrar = findViewById(R.id.bt_cadastro)
 
-        btn_Cadastrar.setOnClickListener { view ->
-            val nome = edit_nome.text.toString().trim()
-            val email = edit_email.text.toString().trim()
-            val senha = edit_senha.text.toString().trim()
+        btnCadastrar.setOnClickListener { view ->
+            val nome = editNome.text.toString().trim()
+            val email = editEmail.text.toString().trim()
+            val senha = editSenha.text.toString().trim()
 
             if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
-                val mensagemErro = "Campos não preenchidos, tente novamente."
+                val mensagemErro = "Campos não preenchidos, tente novamente"
                 Snackbar.make(view, mensagemErro, Snackbar.LENGTH_LONG).show()
             } else {
-                cadastrarUsuario(view, email, senha)
+                cadastrarUsuario(
+                    view,
+                    nome = TODO(),
+                    email = TODO(),
+                    senha = TODO()
+                )
             }
         }
     }
 
-    private fun cadastrarUsuario(view: View, email: String, senha: String) {
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+    private fun cadastrarUsuario(view: View, nome: String, email: String, senha: String) {
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+
+        auth.createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    salvarDadosUsuario()
+
                     val mensagemOK = "Cadastro realizado com sucesso"
-                    Snackbar.make(view, mensagemOK, Snackbar.LENGTH_LONG).show()
+                    val snakbar = Snackbar.make(view, mensagemOK, Snackbar.LENGTH_LONG).show()
 
                     // Volta para o login após 2 segundos
                     view.postDelayed({
@@ -61,4 +76,30 @@ class FormCadastro : AppCompatActivity() {
                 }
             }
     }
+
+    fun salvarDadosUsuario() {
+        val db = FirebaseFirestore.getInstance()
+        val nome = editNome.text.toString().trim()
+        val usuarioID = FirebaseAuth.getInstance().currentUser?.uid
+        val email = FirebaseAuth.getInstance().currentUser?.email
+        if (usuarioID != null && email != null) {
+            val usuarios = hashMapOf(
+                "none" to nome,
+                "email" to email,
+                "uid" to usuarioID
+            )
+            db.collection("Usuarios")
+                .add(usuarios)
+                .addOnSuccessListener { documentReference ->// Add con sucesso
+                    println("Docunento adicionado con ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    println("Enrro ao adicioner documento: $e")
+                }
+        } else {// o usuério não está gutenticado
+            println("Enrro na autenticação")
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/imepac/FormLogin.kt
+++ app/src/main/java/com/example/imepac/FormLogin.kt
@@ -5,32 +5,80 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.imepac.ui.theme.TelaPrincipal
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import kotlin.math.log
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var editEmail: EditText
+    private lateinit var editSenha: EditText
+    private lateinit var btnEntrada: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_form_login)
         getSupportActionBar()?.hide();
+        iniciarComponentes();
 
-        val linkFormCadastro=findViewById<TextView>(R.id.text_tela_cadastro)
-        linkFormCadastro.setOnClickListener{
+        val linkFormCadastro = findViewById<TextView>(R.id.text_tela_cadastro)
+        linkFormCadastro.setOnClickListener {
             val telaCadastro = Intent(this, FormCadastro::class.java)
             startActivity(telaCadastro)
-            Log.d("FormCadastro", "onCreate iniciado")
         }
 
-        val linkTelaPerfil=findViewById<Button>(R.id.bt_entada)
-        linkTelaPerfil.setOnClickListener{
+        val linkTelaPerfil = findViewById<Button>(R.id.bt_entrada)
+        linkTelaPerfil.setOnClickListener {
             val telaPerfil = Intent(this, Tela_Perfil::class.java)
             startActivity(telaPerfil)
         }
+
+        btnEntrada.setOnClickListener {
+            val email = editEmail.text.toString()
+            val senha = editSenha.text.toString()
+            if (email.isEmpty() || senha.isEmpty()) {
+                val mensagemErro = "Campos não preenchidos, tente novamente"
+                Snackbar.make(btnEntrada, mensagemErro, Snackbar.LENGTH_LONG).show()
+            } else {
+                AutenticarUsuario()
+            }
+        }
+    }
+
+    fun AutenticarUsuario() {
+        val email = editEmail.text.toString()
+        val senha = editSenha.text.toString()
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                progressBar.visibility = View.GONE
+
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this@FormLogin, TelaPrincipal::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val mensageErro = task.exception?.message
+                Snackbar.make(findViewById(android.R.id.content), "erro ao autenticar...", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun iniciarComponentes(){
+        progressBar = findViewById(R.id.progressbar)
+        editEmail = findViewById(R.id.edit_email_login)
+        editSenha = findViewById(R.id.edit_senha_login)
+        btnEntrada = findViewById(R.id.bt_entrada)
     }
 }
